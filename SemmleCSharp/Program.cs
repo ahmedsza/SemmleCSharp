aa39e9c84d6df18e3cd0ec233738ab3e6418e2cf
ahmedsza <37135205+ahmedsza@users.noreply.github.com>
2020-06-12 15:47:26 +0200

    Update Program.cs

--- SemmleCSharp/Program.cs
+++ SemmleCSharp/Program.cs
@@ -13,5 +13,16 @@ namespace SemmleCSharp
         {
 
         }
+        
+         static void Demp1()
+        {
+            throw new Exception();
+        }
+
+        static void Demo3()
+        {
+            var abc = "Server = tcp:openhackn82382w0sql.database.windows.net,1433; Initial Catalog = mydrivingDB; Persist Security Info = False; User ID = demousersa1; Password = demo@pass123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; ";
+  }
+        
     }
 }
