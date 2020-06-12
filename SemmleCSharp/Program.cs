1bbce394e257f56ebc64e89ac88121fccdf910d6
ahmeds <ahmeds@ms.com>
2020-06-12 15:08:36 +0200

    initial commit

---
+++ SemmleCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SemmleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
